7b37fca70bf34a4122e3211827923e1cc5f2487a
supermy <springclick@gmail.com>
2015-03-31 23:10:16 +0800

    升级到mongodb3的引擎，空间占用减少2/3 mongod --storageEngine=wiredTiger

--- mymongodb/initdb.sh
+++ mymongodb/initdb.sh
@@ -2,23 +2,23 @@
 #mac sed修改强制要求备份
 
 #初始化复制集1
-cp -f initdb.js initdbi.js
-docker inspect  mymongodb_rs11_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs11/@/g' initdbi.js
-docker inspect  mymongodb_rs12_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs12/@/g' initdbi.js
-docker inspect  mymongodb_rs13_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs13/@/g' initdbi.js
-cat initdbi.js
-mongo  192.168.59.103:27018/test --quiet initdbi.js
+cp -f initdb.js initdbi-1.js
+docker inspect  mymongodb_rs11_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs11/@/g' initdbi-1.js
+docker inspect  mymongodb_rs12_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs12/@/g' initdbi-1.js
+docker inspect  mymongodb_rs13_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs13/@/g' initdbi-1.js
+cat initdbi-1.js
+mongo  192.168.59.103:27018/test --quiet initdbi-1.js
 sleep 3
 
 #初始化复制集2
-cp -f initdb.js initdbi.js
-docker inspect  mymongodb_rs21_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs11/@/g' initdbi.js
-docker inspect  mymongodb_rs22_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs12/@/g' initdbi.js
-docker inspect  mymongodb_rs23_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs13/@/g' initdbi.js
+cp -f initdb.js initdbi-2.js
+docker inspect  mymongodb_rs21_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs11/@/g' initdbi-2.js
+docker inspect  mymongodb_rs22_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs12/@/g' initdbi-2.js
+docker inspect  mymongodb_rs23_1| jsawk -a 'return this[0].NetworkSettings.IPAddress'|awk '{print $1}'|xargs -I@ sed -i bak 's/rs13/@/g' initdbi-2.js
 #更换复制集名称
-sed -i bak 's/rs1/rs2/g' initdbi.js
-cat initdbi.js
-mongo  192.168.59.103:27019/test --quiet initdbi.js
+sed -i bak 's/rs1/rs2/g' initdbi-2.js
+cat initdbi-2.js
+mongo  192.168.59.103:27019/test --quiet initdbi-2.js
 sleep 3
 
 ## 初始化Shard
--- mymongodb/initdbi.js
+++ mymongodb/initdbi.js
@@ -1,6 +1,6 @@
 printjson(1);
-sh.addShard("rs1/172.17.3.205:27017");
-sh.addShard("rs2/172.17.3.198:27017");
+sh.addShard("rs1/172.17.4.28:27017");
+sh.addShard("rs2/172.17.4.23:27017");
 sh.status();
 
 db.runCommand( { listshards : 1 } );
@@ -9,3 +9,19 @@ db.runCommand( { shardcollection : "test.c1",key : {id: 1} } );
 db = db.getSiblingDB("test");
 for(var i=1;i<=2000;i++) db.c1.save({id:i,value1:"12345678"});
 db.c1.stats();
+
+
+
+//
+//#创建用户
+//db = db.getSiblingDB("admin");
+//db.createUser( {
+//    user: "user1",
+//    pwd: "111111",
+//    roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
+//});
+//db.createUser( {
+//    user: "root1",
+//    pwd: "111111",
+//    roles: [ { role: "root", db: "admin" } ]
+//});
