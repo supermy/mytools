abb00f1cd3e53f218e29205abd565de847bf52fe
supermy <springclick@gmail.com>
2015-04-15 15:51:05 +0800

    整合nginx-lua-redis2 and nginx-lua-mysql 为动态调控路由技术准备； tengine的监控检查考虑使用。

---
+++ web+app/mynginx/b.sh
@@ -0,0 +1,10 @@
+#docker build -t jamesmo/mynginx:2.1 .
+
+#http://192.168.59.103:8080/redisfoo
+#http://192.168.59.103:8080/redisget?key=one
+#http://192.168.59.103:8080/redisset?key=one&val=first_value
+
+#http://192.168.59.103:8080/lua-redis-test
+#http://192.168.59.103:8080/lua-mysql-test
+
+#192.168.0.119
