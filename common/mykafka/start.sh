faf7114bc5bd652bbafd27a6e7014ef2fb357a1a
supermy <springclick@gmail.com>
2016-08-26 16:38:57 +0800

    kafka 完美。3个分区，4个线程，一个复制因子；消息保留时间可配置。

--- common/mykafka/start.sh
+++ common/mykafka/start.sh
@@ -1,13 +1,16 @@
+#!/usr/bin/env bash
 fig up -d && fig ps
 
 #tese1
-kafka-topics.sh --create --zookeeper 192.168.59.103:2181 --replication-factor 1 --partitions 1 --topic mykafka
-telnet 192.168.59.103 44447
-kafka-console-consumer.sh --zookeeper 192.168.59.103:2181 --topic mykafka --from-beginning
+kafka-topics.sh --create --zookeeper 192.168.99.101:2181 --replication-factor 1 --partitions 1 --topic mykafka
+telnet 192.168.99.101 44447
+telnet 192.168.99.101 5140
+
+kafka-console-consumer.sh --zookeeper 192.168.99.101:2181 --topic mykafka --from-beginning
 
 #test2
-kafka-topics.sh --create --zookeeper 192.168.59.103:2181 --replication-factor 1 --partitions 1 --topic metadata
-kafka-topics.sh --create --zookeeper 192.168.59.103:2181 --replication-factor 1 --partitions 1 --topic test
+kafka-topics.sh --create --zookeeper 192.168.99.101:2181 --replication-factor 1 --partitions 1 --topic metadata
+kafka-topics.sh --create --zookeeper 192.168.99.101:2181 --replication-factor 1 --partitions 1 --topic test
 java -cp spring-kafka-demo-0.2.0-SNAPSHOT-jar-with-dependencies.jar com.colobu.spring_kafka_demo.Producer
 java -cp spring-kafka-demo-0.2.0-SNAPSHOT-jar-with-dependencies.jar com.colobu.spring_kafka_demo.Consumer
 
