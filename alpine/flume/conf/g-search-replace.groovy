b97ff408d41fd5dbcbdb1a1f40f0de1d028ae8e3
supermy <springclick@gmail.com>
2017-01-04 22:27:03 +0800

    拦截器 进行数据的加密解密 适用于在互联网上传递数据

--- alpine/flume/conf/g-search-replace.groovy
+++ alpine/flume/conf/g-search-replace.groovy
@@ -1,9 +1,36 @@
+import  com.supermy.flume.interceptor.*
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 println head
 println body
 body=body.replace('a','aaa')
 head["newhead"]='abcd'
 
+
+
+String text = "Body 的数据 , I Love BONC"
+
+//
+def key = new SecretKeySpec("123456789987654321".bytes, "AES")
+def c = Cipher.getInstance("AES")
+
+//加密
+c.init(Cipher.ENCRYPT_MODE, key)
+e_text = new String(Hex.encodeHex(c.doFinal(text.getBytes("UTF-8"))))
+
+//解密
+c.init(Cipher.DECRYPT_MODE, key)
+text1 = new String(c.doFinal(Hex.decodeHex(e_text.toCharArray())))
+
+println text
+println e_text
+println text1
+
+
 def resultMap = [:]
+
+//加密数据，用于互联网数据传输
 
 
 resultMap["head"]=head
